b89ef5ed6bff9edf6e4d8458a91e1b1e6e41a17b
peter-819 <peter-819@outlook.com>
2020-10-27 16:31:44 +0800

    added undo&redo buttons, muti-lang

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -3,8 +3,10 @@
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
             <el-submenu index="1">
                 <template slot="title">工程</template>
-                    <el-menu-item index="1-1">保存</el-menu-item>
-                    <el-menu-item index="1-2">打开</el-menu-item>
+                    <el-menu-item index="1-1">新建</el-menu-item>
+                    <el-menu-item index="1-2">保存</el-menu-item>
+                    <el-menu-item index="1-3">打开</el-menu-item>
+                    <el-menu-item index="1-4">关闭</el-menu-item>
             <el-submenu index="1-3">
                 <template slot="title">最近</template>
                     <el-menu-item index="1-3-1">Project1</el-menu-item>
@@ -13,21 +15,37 @@
             </el-submenu>
           </el-submenu>
 
-
           <el-submenu index="2">
-            <template slot="title">点云</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
+            <template slot="title">图像</template>
+            <el-menu-item index="2-1">打开</el-menu-item>
+            <el-menu-item index="2-2">降噪</el-menu-item>
+            <el-menu-item index="2-3">增强</el-menu-item>
+            <el-menu-item index="2-4">生成点云</el-menu-item>
+            <el-menu-item index="2-4">关闭</el-menu-item>
           </el-submenu>
 
-          <el-menu-item index="3" disabled>消息中心</el-menu-item>
+          <el-submenu index="3">
+            <template slot="title">点云</template>
+            <el-menu-item index="3-1">打开点云</el-menu-item>
+            <el-menu-item index="3-2">降噪</el-menu-item>
+            <el-menu-item index="3-3">平滑</el-menu-item>
+            <el-menu-item index="3-4">fuse...</el-menu-item>
+            <el-menu-item index="3-5">三维重建</el-menu-item> 
+            <el-menu-item index="3-6">关闭</el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">模型</template>
+            <el-menu-item index="4-1">打开模型</el-menu-item>
+            <el-menu-item index="4-2">检测缺陷</el-menu-item>
+            <el-menu-item index="4-3">关闭模型</el-menu-item>
+          </el-submenu>
+
+          <el-button-group style="margin-top:10px; right:30px; position: absolute;">
+            <el-button icon="el-icon-arrow-left"></el-button>
+            <el-button icon="el-icon-arrow-right"></el-button>
+          </el-button-group>
+
         </el-menu>
         </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -7,13 +7,26 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import VueI18n from 'vue-i18n'
+
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale:'cn',
+  messages:{
+    'cn': require('./lang/cn'),
+    'en': require('./lang/en')
+  }
+})
+export default i18n
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n:i18n,
   components: { App },
   template: '<App/>'
 })
